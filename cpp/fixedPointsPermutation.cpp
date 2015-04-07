48489b177bc6b72d25ef5c320c088e00a4d6ef6e
Nalin_C <nalin.chhibber@gmail.com>
2015-04-07 15:57:46 +0530

    Update fixedPointsPermutation.cpp

--- cpp/fixedPointsPermutation.cpp
+++ cpp/fixedPointsPermutation.cpp
@@ -17,7 +17,7 @@ int fixedPointsPermutation(std::vector<int> permutation) {
 
   int result = 0;
 
-  for (int i = 1; i <= permutation.size(); i++) {
+  for (int i = 0; i < permutation.size(); i++) {
     if (permutation[i] == i + 1) {
       result++;
     }
