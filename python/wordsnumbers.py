7b62e789c3ba77125c04134a0d6d56fa6c3d6a77
agus makmun <summon.agus@gmail.com>
2016-06-06 03:43:10 +0700

    Create wordsnumbers.py

---
+++ python/wordsnumbers.py
@@ -0,0 +1,36 @@
+#Question: https://python.web.id/blog/words-to-numbers-cf/
+
+def wordsnumbers(textnum, numwords={}):
+    if not numwords:
+      units = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen",
+      ]
+      tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+      scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+      numwords["and"] = (1, 0)
+      for idx, word in enumerate(units):    numwords[word] = (1, idx)
+      for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+      for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+          raise Exception("Illegal word: " + word)
+
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
+    return result + current
+
+
+'''
+>>> wordsnumbers("five thousand one hundred")
+5100
+>>> wordsnumbers("one hundred twenty five")
+125
+'''
